b7a51b1c941d831ff86aa810d9eeb489f72a71b7
takyam <takayuki.yamaguch.m@gmail.com>
2012-10-08 17:07:30 +0900

    add coffeescript-php to submodule

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ Autoloader::add_core_namespace('CoffeeScript');
 Autoloader::add_classes(array(
     // Alias to Coffeepress class
     'Coffee\\Coffee'                => __DIR__.'/classes/coffee.php',
-    'CoffeeScript\\Init'            => __DIR__.'/vendor/coffeescript/src/CoffeeScript/Init.php',
-    'CoffeeScript]\\Compiler'       => __DIR__.'/vendor/coffeescript/src/CoffeeScript/Compiler.php',
+    'CoffeeScript\\Init'            => __DIR__.'/vendor/coffeescript-php/src/CoffeeScript/Init.php',
+    'CoffeeScript]\\Compiler'       => __DIR__.'/vendor/coffeescript-php/src/CoffeeScript/Compiler.php',
 ));
 \CoffeeScript\Init::load();
